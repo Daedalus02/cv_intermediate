6dafb359fd5f31b601f5170e0f488c764c37d09f
Daedalus02 <mattia.scantamburlo02@gmail.com>
2025-04-23 16:17:31 +0200

    aggiunto Template

---
+++ src/Template_matching.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <opencv2/opencv.hpp>
+#include <cmath> 
+# include "../include/graphic.h"
+
+// Declaring the namespace with the filesystem of the current OS.
+namespace fs = std::filesystem;
+
+// Generate scales between minScale and maxScale with step equal to step.
+std::vector<double> generateScales(double minScale, double maxScale, double step) {
+    std::vector<double> scales;
+    for (double s = minScale; s <= maxScale; s += step) {
+        scales.push_back(s);
+    }
+    return scales;
+}
+
+// Generate angles between minAngle and maxAngle with step equal to step.
+std::vector<double> generateAngles(double minAngle, double maxAngle, double step) {
+    std::vector<double> angles;
+    for (double a = minAngle; a <= maxAngle; a += step) {
+        angles.push_back(a);
+    }
+    return angles;
+}
+
+// Function to crop image to non-black region using mask
+cv::Mat cropToMask(const cv::Mat& image, const cv::Mat& mask) {
+    // Find bounding box of non-zero pixels in mask
+    cv::Rect bbox = cv::boundingRect(mask);
+    // Crop both image and mask to this region
+    return image(bbox).clone();
+}
+
+cv::Mat normalizeMinMax(const cv::Mat& input) {
+    cv::Mat normalized;
+    // Split into channels
+    std::vector<cv::Mat> channels;
+    cv::split(input, channels);
+
+    // Normalize each channel separately
+    for (int i = 0; i < 3; i++) {
+        cv::normalize(channels[i], channels[i], 0, 255, cv::NORM_MINMAX);
+    }
+
+    // Merge back
+    cv::merge(channels, normalized);
+    return normalized;
+}
+
+//view_0_003_color
+const   std::string models_paths[] = {
+    "../data/004_sugar_box/models/view_0_003_color.png"
+};
+
+
+const std::string mask_paths[] = {
+
+    "../data/004_sugar_box/models/view_0_003_mask.png"
+
+};
+
+int main() {
+
+    // Defining the path of the image to test the algorithm on.
+    std::string immagineTestPath = "../data/004_sugar_box/test_images/4_0001_000956-color.jpg";
+    // Defining the path of the folder with all the models images.
+    std::string modelsFolderPath = "../data/004_sugar_box/models/";
+
+    // Defining the confidence threshold for the test.
+    double confidenceThreshold = 0.3;
+    // Defining a vector of double values for all the scales to test the template on.
+    std::vector<double> scales = generateScales(0.75, 1.5, 0.25);
+    // Defining a vector of double values for all the inclinations to test the template on.
+    std::vector<double> angles = generateAngles(-60, 60, 15); 
+
+    // Loading the image in both color scale and grey scale to test both.
+    cv::Mat imgTestColor = cv::imread(immagineTestPath, cv::IMREAD_COLOR);
+    cv::Mat imgTestGray = cv::imread(immagineTestPath, cv::IMREAD_GRAYSCALE);
+
+    // Testing the validity of both the Mat objects.
+    if (imgTestColor.empty() || imgTestGray.empty()) {
+        // If one of the images was not present in the path, print error message and quit.
+        std::cerr << "Errore nel caricamento dell'immagine di test." << std::endl;
+        return -1;
+    }
+
+
+    cv::blur(imgTestColor, imgTestColor, cv::Size(4,4));
+    imgTestColor = normalizeMinMax(imgTestColor);
+    cv::namedWindow("Normalize");
+    cv::imshow("Normalize", imgTestColor);
+
+    double maxValGlobal = -1;
+    // Point object to store the point of higher probability for a match. 
+    cv::Point maxLocGlobal;
+    // Size object for storing the size of the maxLocGlobal point. 
+    cv::Size templateSizeGlobal;
+
+    double best_angle = 90;
+    double best_scale = 2;
+    int best_k = 1;
+    cv::Mat best_model = cv::imread(immagineTestPath);
+
+
+    // Iterating through all the possible models images in the directory.
+    for (int i = 0; i < size(models_paths); i++) {   //size(models_paths)
+
+        // Creating a variable of type Mat with the loaded model image in grayscale.
+        cv::Mat templColorOriginal = cv::imread(models_paths[i], cv::IMREAD_COLOR);
+        cv::Mat templMask = cv::imread(mask_paths[i], cv::IMREAD_GRAYSCALE);
+        // Ensure mask is binary (0 or 255)
+        cv::threshold(templMask, templMask, 127, 255, cv::THRESH_BINARY);
+
+
+        // Crop template to mask region
+        cv::Mat templCropped = cropToMask(templColorOriginal, templMask);
+        cv::Mat maskCropped = cropToMask(templMask, templMask);
+        templMask = maskCropped.clone();
+        templColorOriginal = templCropped.clone();
+
+        cv::Mat mask3ch;
+        cv::cvtColor(maskCropped, mask3ch, cv::COLOR_GRAY2BGR);
+        cv::bitwise_and(templCropped, mask3ch, templCropped);
+        templCropped = normalizeMinMax(templCropped);
+
+        //cv::imshow(models_paths[i], templGrayOriginal);
+
+        // Checking if the loaded model image is valid.
+        if (!templCropped.empty()) {
+            for(int k = -4; k < 4; k++){
+                float scale = 1.0f - k * 0.1f;
+
+                for(int i = 0; i < templCropped.rows; i++){
+                        for(int j = 0; j < templCropped.cols; j++){
+                            cv::Vec3b color = templCropped.at<cv::Vec3b>(i,j);
+                            if(color[0]*scale < 255 && color[1]*scale < 255 && color[2]*scale < 255){
+                                templCropped.at<cv::Vec3b>(i,j)[0] = templCropped.at<cv::Vec3b>(i,j)[0]*scale;
+                                templCropped.at<cv::Vec3b>(i,j)[1] = templCropped.at<cv::Vec3b>(i,j)[1]*scale;
+                                templCropped.at<cv::Vec3b>(i,j)[2] = templCropped.at<cv::Vec3b>(i,j)[2]*scale;
+                            }
+                        }
+                    }
+            
+            cv::namedWindow("Cropped");
+            cv::imshow("Cropped", templCropped);
+
+            
+            // Iterating over all the possible scales.
+            for (double scale : scales) {
+                // Resizing the template image with current scale configuration.
+                cv::Mat templColorResized;
+                cv::resize(templCropped, templColorResized, cv::Size(), scale, scale);
+
+                // Combining all the possible angles for the rotation of the template.
+                for (double angle : angles) {
+                    // Rotating the current scale image.
+                    graphic templColorRotated = graphic(templColorResized);
+                    templColorRotated.rotateImage(angle);
+
+                    // Checking if the current test image is bigger in width and height than the template.
+                    if (imgTestColor.cols >= templColorRotated.cols() && imgTestColor.rows >= templColorRotated.rows()) {
+
+                        
+                        // Storing the result if the template sliding process in "result" variable.
+                        cv::Mat result;
+                        // Using the TM_CCOEFF_NORMED method.
+                        cv::matchTemplate(imgTestColor, templColorRotated.getImage(), result, cv::TM_CCOEFF_NORMED);
+                        // Storing the mininmum and maximum values and location among all the possible positions.
+                        double minVal; double maxVal;
+                        cv::Point minLoc; cv::Point maxLoc;
+                        cv::minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc);
+
+                        // Checking if the maximum value found in the current configuration is better than the global one.
+                        if (maxVal > maxValGlobal) {
+                            // Fixing the new global maximum value amd position.
+                            maxValGlobal = maxVal;
+                            maxLocGlobal = cv::Point(cvRound(maxLoc.x), cvRound(maxLoc.y));
+                            // Storin git merge dev-mattia g the size of the current template.
+                            templateSizeGlobal = cv::Size(templColorRotated.cols(), templColorRotated.rows());
+                            best_scale = scale;
+                            best_angle = angle;
+                            best_model = templCropped.clone();
+                            best_k = k;
+                        }
+                    }
+                }
+            }
+        }
+      }
+    }
+
+    // Checking whether the maximum value found among all the configuration is good enough.
+    if (maxValGlobal > confidenceThreshold) {
+        // Painting the rectangle around the position of higher matching value. 
+        cv::Point topLeft = maxLocGlobal;
+        cv::Point bottomRight(topLeft.x + templateSizeGlobal.width, topLeft.y + templateSizeGlobal.height);
+        cv::rectangle(imgTestColor, topLeft, bottomRight, cv::Scalar(0, 255, 0), 2);
+        std::cout << "Object detected with confidence score: " << maxValGlobal << std::endl;
+    } else {
+        std::cout << "No suffuciently good corrispondence found among all templates (threshold: " << confidenceThreshold << "). Max confidence: " << maxValGlobal << std::endl;
+    }
+
+    std::cout<<"Best angle is: "<<best_angle<<std::endl;
+    std::cout<<"Best scale is: "<<best_scale<<std::endl;
+    std::cout<<"Best k is: "<<best_k<<std::endl;
+    cv::namedWindow("Best template");
+    cv::imshow("Best template", best_model);
+    cv::imshow("Image with detected object", imgTestColor);
+    cv::waitKey(0);
+
+    return 0;
+}
