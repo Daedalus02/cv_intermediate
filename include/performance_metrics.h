44418aa7454a7979b962c69997cdf4e97b2f15df
Ale10chine <alex10.chinello@gmail.com>
2025-04-24 17:03:51 +0200

    First upload of Matrics

---
+++ include/performance_metrics.h
@@ -0,0 +1,66 @@
+// Include guards.
+#ifndef PERFOMANCE_METRICS_H
+#define PERFOMANCE_METRICS_H
+
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/core/types.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/imgproc.hpp"
+#include <string.h>
+#include <vector>
+#include <fstream>
+#include <string>
+
+
+class PerfomanceMetrics{
+
+    public:
+        // CONSTRUCTORS:
+        PerfomanceMetrics(const std::string &path_pred_labels, const std::string &path_true_labels) : path_pred_labels(path_pred_labels),
+                                                                                                      path_true_labels(path_true_labels),
+                                                                                                      sugar_p(2),
+                                                                                                      mustard_p(2),
+                                                                                                      power_drill_p(2),
+                                                                                                      sugar_t(2),
+                                                                                                      mustard_t(2),
+                                                                                                      power_drill_t(2)
+        {}
+
+        // FUNCTION MEMBERS:
+        double compute_detection_accuracy();
+        
+    private:
+        // DATA MEMBER:
+        // paths to file of predicted labels and true labels for which apply the metrics
+        std::string path_pred_labels, path_true_labels; 
+
+        std::vector<std::pair<int, int>> sugar_p, mustard_p, power_drill_p;
+        std::vector<std::pair<int, int>> sugar_t, mustard_t, power_drill_t;
+        // PRIVATE FUNCTION:
+        
+        // This function compute the mean intersection over union.
+        //double compute_mIoU();
+
+};
+
+// HELPER FUNCTIONS:
+    // This function returns the two point <x_max, y_max>, <x_min, y_min> given a line.
+    //std::vector<cv::Point>
+    void parser(const std::string& path , std::vector<std::pair<int, int>>& sugar, std::vector<std::pair<int, int>>& mustard, std::vector<std::pair<int, int>>& power_drill);
+    
+
+    // Possible helper function to write in a text file the perfomances.
+
+    // DEBUG:
+
+    void printValue(std::vector<std::pair<int, int>> v1, std::vector<std::pair<int, int>> v2, std::vector<std::pair<int, int>> v3);
+
+    
+
+
+
+
+#endif
---
+++ lib/performance_metrics.cpp
@@ -0,0 +1,89 @@
+#include "../include/performance_metrics.h"
+
+
+// FUNCTION MEMBERS
+double PerfomanceMetrics:: compute_detection_accuracy(){
+
+    // Parser for predicted labels
+    parser(this->path_pred_labels, this->sugar_p, this->mustard_p, this->power_drill_p);
+    
+    // Parser for true labels
+    parser(this->path_true_labels, this->sugar_t, this->mustard_t, this->power_drill_t);
+
+    std::cout<<"Predicted labels:\n\n";
+    printValue(this->sugar_p, this->mustard_p, this->power_drill_p);
+
+    std::cout<<"True labels:\n\n";
+    printValue(this->sugar_t, this->mustard_t, this->power_drill_t);
+    // Calculus of the metrics
+
+    return 0;
+
+}
+
+// HELPER FUCNTIONS
+//std::vector<cv::Point>
+void parser(const std::string& path , std::vector<std::pair<int, int>>& sugar, std::vector<std::pair<int, int>>& mustard, std::vector<std::pair<int, int>>& power_drill){
+
+    std::ifstream file(path);
+
+    if(!file.is_open()){
+        std::cerr<< "Impossibile to open the file \n";
+        return;
+    }
+    std::string line;
+
+    while(std::getline(file,line)){
+
+        std::vector<std::string> words;
+        std::istringstream iss(line);
+        std::string word;
+        
+        // Dividing the line using space as separator
+        while(iss >> word) {
+            words.push_back(word);
+        }
+
+        if(words[0] == "004_sugar_box"){
+
+            sugar[0] = std::make_pair(std::stoi(words[1]), std::stoi(words[2]));
+            sugar[1] = std::make_pair(std::stoi(words[3]), std::stoi(words[4]));
+
+        }else if(words[0] == "006_mustard_bottle"){
+
+            mustard[0] = std::make_pair(std::stoi(words[1]), std::stoi(words[2]));
+            mustard[1] = std::make_pair(std::stoi(words[3]), std::stoi(words[4]));
+
+        }else if (words[0] == "035_power_drill")
+        {
+            power_drill[0] = std::make_pair(std::stoi(words[1]), std::stoi(words[2]));
+            power_drill[1] = std::make_pair(std::stoi(words[3]), std::stoi(words[4]));
+        }
+        
+        // closed automatically the file open thanks to the distructor of ifstream
+        
+    }
+
+}
+
+void printValue(std::vector<std::pair<int, int>> v1, std::vector<std::pair<int, int>> v2, std::vector<std::pair<int, int>> v3){
+
+    std::cout << "Sugar Coord:" << std::endl;
+    for (const auto& p : v1) {
+        std::cout << "(" << p.first << ", " << p.second << ") ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Mustard Coord:" << std::endl;
+    for (const auto& p : v2) {
+        std::cout << "(" << p.first << ", " << p.second << ") ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Power Drill Coord:" << std::endl;
+    for (const auto& p : v3) {
+        std::cout << "(" << p.first << ", " << p.second << ")";
+    }
+    std::cout << "\n\n";
+
+}
